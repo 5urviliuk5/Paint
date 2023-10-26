078fb2d6048850310bd5dc5a61d5e3fabca36a4f
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2023-10-26 22:06:23 +0300

    end

--- Assets/Scripts/Painter.cs
+++ Assets/Scripts/Painter.cs
@@ -19,15 +19,45 @@ public class Painter : MonoBehaviour
             AddPoint();
         }
 
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name.Equals("BlueColor"))
+                {
+                    CreateNewLine(Color.blue);
+                }
+                else if (hit.collider.gameObject.name.Equals("RedColor"))
+                {
+                    CreateNewLine(Color.red);
+                }
+                else if (hit.collider.gameObject.name.Equals("GreenColor"))
+                {
+                    CreateNewLine(Color.green);
+                }
+            }
+        }
     }
 
     void AddPoint()
     {
-        line.positionCount++;
-
         var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         worldPos.z = 0;
-
+        line.positionCount++;
         line.SetPosition(line.positionCount - 1, worldPos);
+    }
+
+    void CreateNewLine(Color color)
+    {
+        GameObject newLineObject = new GameObject("LineRenderer");
+        line = newLineObject.AddComponent<LineRenderer>();
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        line.startColor = color;
+        line.endColor = color;
+        line.startWidth = 0.2f;
+        line.endWidth = 0.2f;
     }
 }
