5b26c9b57a771b6cc2375db9fc7c152067bc414d
Surviliuks <145654904+5urviliuk5@users.noreply.github.com>
2023-10-26 19:56:00 +0300

    Nothing

---
+++ Assets/Scripts/Painter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painter : MonoBehaviour
+{
+    public GameObject lineObject;
+    LineRenderer line;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            line = Instantiate(lineObject).GetComponent<LineRenderer>();
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            AddPoint();
+        }
+
+    }
+
+    void AddPoint()
+    {
+        line.positionCount++;
+
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+
+        line.SetPosition(line.positionCount - 1, worldPos);
+    }
+}
